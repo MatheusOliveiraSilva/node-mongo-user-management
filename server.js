ed4c35b536ba2e6eedcd5d9161cff44715eda21f
Matheus Oliveira <matheus.oliveira.s@protonmail.com>
2020-07-20 22:21:37 -0300

    router added and jwtPrivateKey error handler

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const config = require('config')
+
+if (!config.get('jwtPrivateKey')) {
+  console.error('FATAL ERROR: jwtPrivateKey is not defined.')
+  process.exit(1)
+}
 
 const app = express()
 
+const users = require('./users/user.controller')
+const secure = require('./secure_content/content')
+
 app.use(express.json())
 
-app.use('/api', (req, res) => {
-  res.send([1, 3, 3, 1])
-})
-
-app.use('/users', require('./users/user.controller'))
+app.use('/users', users)
+app.use('/secure', secure)
 
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
 }
 
 mongoose.connect('mongodb://localhost/node-mongo-user-management', options)
 
 const port = process.env.PORT || 1331
-app.listen(port, () => console.log(`Server listenning on port ${port}`))
+app.listen(port, () => console.log(`Server listening on port ${port}`))

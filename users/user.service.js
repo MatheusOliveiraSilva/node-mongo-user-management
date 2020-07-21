ee319a74c5c628f2b3cc8432fcce05b0c985fd3c
Matheus Oliveira <matheus.oliveira.s@protonmail.com>
2020-07-20 22:32:36 -0300

    methods of request added

--- users/user.service.js
+++ users/user.service.js
@@ -1,10 +1,45 @@
 const User = require('./user.model')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const config = require('config')
 
 module.exports = {
-  async authenticate() {},
-  async getAll() {},
-  async getById() {},
-  async create() {},
+  async authenticate({ username, password }) {
+    const user = await User.findOne({ username })
+    if (user && (await bcrypt.compare(password, user.hash))) {
+      const token = user.generateAuthToken()
+
+      return {
+        ...user.toJSON(),
+        token,
+      }
+    }
+  },
+  async getAll() {
+    return await User.find()
+  },
+  async getById(id) {
+    return await User.findById(id).select('-hash')
+  },
+  async create(userParam) {
+    if (await User.findOne({ username: userParam.username })) {
+      throw `Username "${userParam.username}" is already taken...`
+    }
+
+    const user = new User(userParam)
+
+    if (userParam.password) {
+      user.hash = await bcrypt.hash(userParam.password, 10)
+    }
+
+    await user.save()
+
+    const token = user.generateAuthToken()
+
+    return token
+  },
   async update() {},
-  async delete() {},
+  async delete(id) {
+    return User.findOneAndRemove(id)
+  },
 }

ac5cb6b1ba79c73dc93ffd967ca77f2071644597
Matheus Oliveira <matheus.oliveira.s@protonmail.com>
2020-07-20 22:28:14 -0300

    schema methods and schema's properties added

--- users/user.model.js
+++ users/user.model.js
@@ -1,14 +1,41 @@
 const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+const config = require('config')
 
 const Schema = mongoose.Schema
 
 const schema = new Schema({
-  username: {},
-  hash: {},
-  firstName: {},
-  lastName: {},
-  createdDate: {},
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  hash: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now,
+  },
+  isAdmin: Boolean,
 })
+
+schema.methods.generateAuthToken = function () {
+  const token = jwt.sign(
+    { _id: this._id, isAdmin: this.isAdmin },
+    config.get('jwtPrivateKey')
+  )
+  return token
+}
 
 schema.set('toJSON', {
   virtuals: true,
